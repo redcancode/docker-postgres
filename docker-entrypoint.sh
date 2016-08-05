a997a4a01b230d86fe402203973ffb768eaeffbb
Istvan Bodnar <istvan.bodnar@supercharge.io>
2016-08-05 14:46:06 +0200

    Make postgres listen on all interfaces

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-if [ ! -e /var/lib/postgresql/data/PG_VERSION ]; then
+if [ ! -e $PGDATA/PG_VERSION ]; then
   su-exec postgres initdb
+  su-exec postgres sed -i -e "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" $PGDATA/postgresql.conf
 fi
 
 su-exec postgres postgres
