5ceaffac11da96918774cfc7313649e19bfbc16f
Istvan Bodnar <istvan.bodnar@supercharge.io>
2016-08-05 16:02:39 +0200

    Trust everyone who can access the server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@
 if [ ! -e $PGDATA/PG_VERSION ]; then
   su-exec postgres initdb
   su-exec postgres sed -i -e "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" $PGDATA/postgresql.conf
+  su-exec postgres echo "host all all 0.0.0.0/0 trust" >> $PGDATA/pg_hba.conf
 fi
 
 su-exec postgres postgres
